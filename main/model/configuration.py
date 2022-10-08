c67577669ade274d58de6767a2051d0c43f4971e
Lukas Brauckmann <lukas.brauckmann@gmail.com>
2022-10-08 14:42:00 +0200

    Remove unused code

--- main/model/configuration.py
+++ main/model/configuration.py
@@ -2,18 +2,12 @@ import json
 import os
 from typing import List, Set
 
-from sharepp import Coin, SharePP
+from sharepp import SharePP
 from main.model.errors import ConfigurationException
-from main.model.named_list import NamedList
 from main.model.asset.etf import ETF
 from main.model.asset.DeepAsset import DeepAsset
-from main.model.asset.FlatAsset import FlatAsset
 from main.model.classification.category import Category
 from main.model.classification.classification import Classification
-from main.model.investment.BaseInvestment import BaseInvestment
-from main.model.investment.CryptoInvestment import CryptoInvestment
-from main.model.investment.ETFInvestment import ETFInvestment
-from main.model.investment.TERInvestment import TERInvestment
 
 etfs: "dict[str,ETF]" = {}
 classifications: List[Classification] = []
@@ -92,56 +86,3 @@ def parse_classifications(asset: DeepAsset, asset_config):
         asset.classifications.append(classification)
 
     # parse_investments(asset, asset_config)
-
-    def parse_investments(asset, asset_config):
-        investments_config = asset_config["investments"]
-        for investment_id in investments_config:
-            investment: BaseInvestment
-            name = investments_config[investment_id]["name"]
-            enabled = investments_config[investment_id]["enabled"]
-            quantity = investments_config[investment_id]["quantity"]
-            if investments_config[investment_id]["type"] == "etf":
-                categories_str = investments_config[investment_id]["categories"].split(
-                    ","
-                )
-                if "ter" in investments_config[investment_id]:
-                    ter = investments_config[investment_id]["ter"]
-                    investment = TERInvestment(
-                        investment_id, enabled, name, quantity, ter
-                    )
-                else:
-                    investment = ETFInvestment(investment_id, enabled, name, quantity)
-
-                categories_list: NamedList[Category] = NamedList()
-                for category_str in categories_str:
-                    categories_list.append(asset.get_all_categories().get(category_str))
-
-                asset.investments[investment] = categories_list
-            else:
-                format_string = "Unknown investment type '{type}'!"
-                raise ConfigurationException(
-                    format_string.format(type=investments_config[investment_id]["type"])
-                )
-
-    def parse_allocated_investments(asset: FlatAsset, asset_config):
-        investments_config = asset_config["investments"]
-        for investment_id in investments_config:
-            investment: BaseInvestment
-            name = investments_config[investment_id]["name"]
-            enabled = investments_config[investment_id]["enabled"]
-            quantity = investments_config[investment_id]["quantity"]
-            allocation = float(investments_config[investment_id]["allocation"])
-            if investments_config[investment_id]["type"] == "etf":
-                investment = ETFInvestment(
-                    investment_id, enabled, name, quantity, allocation
-                )
-            elif investments_config[investment_id]["type"] == "crypto":
-                coin = Coin(investment_id)
-                investment = CryptoInvestment(coin, enabled, name, quantity, allocation)
-            else:
-                format_string = "Unknown investment type '{type}'!"
-                raise ConfigurationException(
-                    format_string.format(type=investments_config[investment_id]["type"])
-                )
-
-            asset.investments.append(investment)
